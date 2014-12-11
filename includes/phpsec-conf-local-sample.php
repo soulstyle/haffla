bd6b5f7e30f906fcdc0782e771e551f8dac20c22
David Sahlström <david.s@soulstyle.se>
2014-12-11 02:50:38 +0100

    Added local-config-sample file

---
+++ includes/phpsec-conf-local-sample.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Inloggningsdetaljer för lokal databas - Dev server
+ */  
+define("HOST", "localhost");     				// IP/Hostname till databasen som du vill ansluta till
+define("USER", "");    					// Användarnamn i databasen 
+define("PASSWORD", "");   	 	// Lösenord till databasen
+define("DATABASE", "haffla");    			// Databasens namn
+
+define("HOST2", "localhost");
+define("USER2", "");
+define("PASSWORD2", "");
+define("DATABASE2", "haffla_list");
